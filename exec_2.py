c59e2f81595d5f8a695b7bbb121a3f000476c545
Fabiano Oliveira <fabiano-oliveira@hotmail.com>
2023-04-21 09:58:05 -0300

    commit exec_2

---
+++ exec_2.py
@@ -0,0 +1,14 @@
+"2) Desenvolver um algoritmo que leia a altura de 15 pessoas. Este programa deverá calcular e mostrar"
+"a. A menor altura do grupo;"
+"b. A maior altura do grupo;"
+maiorAltura = 0
+menorAltura = 1000
+for c in range(1, 5):
+    altura = float(input('Digite o valor da altura {}: '.format(c)))
+    if altura > maiorAltura:
+        maiorAltura = altura
+    if altura < menorAltura:
+        menorAltura = altura
+print('Maior altura: {}'.format(maiorAltura))
+print('Menor altura: {}'.format(menorAltura))
+
